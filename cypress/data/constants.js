faadbb9d71d6891bc5ef5afa41883359f211eec3
Andreeas Coroian <andreeas.coroian@gmail.com>
2022-07-07 16:20:56 +0300

    refactored assert email field error per in and out options
    refactored method to work on both mac or windows
    
    refactored method to work on both mac or windows
    
    Refactor
    
    refactor

--- cypress/data/constants.js
+++ cypress/data/constants.js
@@ -4,7 +4,8 @@ let careersConstants = {
     FIRST_NAME_TEXT: "Firstname",
     LAST_NAME_TEXT: "Lastname",
     SUBMIT_TEXT: "Submit",
-    PLEASE_FILL_OUT_THIS_FIELD_MESSAGE: "Please fill out this field."
+    PLEASE_FILL_OUT_THIS_FIELD_MESSAGE: "Please fill out this field.",
+    PLEASE_FILL_IN_THIS_FIELD_MESSAGE: "Please fill in this field."
 };
 
 let homepageConstants = {
--- cypress/pages/Careers.js
+++ cypress/pages/Careers.js
@@ -9,13 +9,13 @@ export class Careers {
     };
 
     clickOnApplyForThisPosition() {
-        commonMethods.waitForDocumentToBeLoaded('2244');
+        cy.wait(500)
         getIframeBody().find(careersSelectors.APPLY_FOR_THIS_POSITION_BUTTON).scrollIntoView();
         getIframeBody().find(careersSelectors.APPLY_FOR_THIS_POSITION_BUTTON).click();
     };
 
     answerToFirstQuestions() {
-        commonMethods.waitForDocumentToBeLoaded('486');
+        cy.wait(500)
         getIframeBody().find(careersSelectors.YES_ANSWER).eq(0).click();
         getIframeBody().find(careersSelectors.YES_ANSWER).eq(1).click();
     };
@@ -54,46 +54,44 @@ export class Careers {
 
     getEmailFieldHtmlError() {
         getIframeBody().find(careersSelectors.EMAIL_FIELD).invoke('prop', 'validationMessage')
-            .should('equal', careersConstants.PLEASE_FILL_OUT_THIS_FIELD_MESSAGE);
+            .then($el => {
+                const x = $el.toString()
+                cy.wrap(x).as('y')
+                if (x.includes('out')) {
+                    cy.get('@y').should('contain', careersConstants.PLEASE_FILL_OUT_THIS_FIELD_MESSAGE)
+                } else {
+                    cy.get('@y').should('contain', careersConstants.PLEASE_FILL_IN_THIS_FIELD_MESSAGE)
+                };
+            })
     };
 
     navigateToPersonalInformationPage() {
         this.answerToFirstQuestions();
         this.clickOnContinue();
-        commonMethods.waitForDocumentToBeLoaded('523');
+        cy.wait(500);
         this.clickOnContinue();
-        commonMethods.waitForDocumentToBeLoaded('1291');
+        cy.wait(500);
         this.selectGender();
         this.selectRace();
         this.clickOnContinue();
-        commonMethods.waitForDocumentToBeLoaded('1132');
+        cy.wait(500);
         this.selectVeteranChoice();
         this.clickOnContinue();
-        commonMethods.waitForDocumentToBeLoaded('1641');
+        cy.wait(500);
         this.clickOnContinue();
-        commonMethods.waitForDocumentToBeLoaded('2641');
+        cy.wait(500);
     };
 }
 
 const getIframeDocument = () => {
     return cy
         .get('#gnewtonIframe')
-        // Cypress yields jQuery element, which has the real
-        // DOM element under property "0".
-        // From the real DOM iframe element we can get
-        // the "document" element, it is stored in "contentDocument" property
-        // Cypress "its" command can access deep properties using dot notation
-        // https://on.cypress.io/its
         .its('0.contentDocument').should('exist')
 };
 
 const getIframeBody = () => {
-    // get the document
     return getIframeDocument()
-        // automatically retries until body is loaded
         .its('body').should('not.be.undefined')
-        // wraps "body" DOM element to allow
-        // chaining more Cypress commands, like ".find(...)"
         .then(cy.wrap)
 };
 
